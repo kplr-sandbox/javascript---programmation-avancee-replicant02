d532ddc549ac40eef66daa7a22c471946be32825
replicant02 <128981316+replicant02@users.noreply.github.com>
2023-03-28 10:03:16 +0000

    exercice age final

--- Day-02/playground/javascript/script.js
+++ Day-02/playground/javascript/script.js
@@ -4,10 +4,40 @@ affiche "accepté" si l'age est supérieur ou égal à 18 ans, et "refusé" sino
 */
 
 const ageMajorite = 18;
-let ageUtilisateur = prompt("Merci d'indiquer votre âge");
+let ageUtilisateur = parseInt(prompt("Merci d'indiquer votre âge"));
 
+/*
 if (parseInt(ageUtilisateur) >= ageMajorite) {
   alert("Vous êtes accepté !");
 } else {
   alert("Désolé vous ne pouvez pas rentrer");
 }
+*/
+
+/*
+let message =
+  ageUtilisateur >= ageMajorite
+    ? "Vous êtes accepté !"
+    : "Désolé vous ne pouvez pas rentrer";
+
+alert(message);
+*/
+
+// on ajoute un age maximum
+const ageMaximum = 70;
+
+// on vérifie aussi que l'âge supérieur à zéro
+/*
+let message =
+  ageUtilisateur > 1 &&
+  ageUtilisateur <= ageMaximum &&
+  ageUtilisateur >= ageMajorite
+    ? "Vous êtes accepté !"
+    : "Désolé vous ne pouvez pas rentrer";
+*/
+
+let message =
+  (ageUtilisateur >= ageMajorite && "Vous êtes accepté") ||
+  (ageUtilisateur < ageMajorite && "Désolé vous ne pouvez pas rentrer");
+
+alert(message);
